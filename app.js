d5a1a563f340b74e5ca9af3e179faad4fb346e4f
eunicesantiago-collab <eunice.santiago@billease.ph>
2026-06-18 18:28:38 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Billease Report Loaded");
+const SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSRR37cSPX3g8O-Vmm6VqkvpdnyLL5gVU4tP66oj-cxvNrfww6NEqDzoX070qle6w/pub?gid=2043204496&single=true&output=csv";
+
+fetch(SHEET_URL)
+  .then(response => response.text())
+  .then(csvText => {
+    console.log("Google Sheet CSV Loaded:");
+    console.log(csvText);
+  })
+  .catch(error => {
+    console.error("Error loading Google Sheet:", error);
+  });
