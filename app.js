4e58f6a157fc7b2dc2bb1a426214caef5408cb59
eunicesantiago-collab <eunice.santiago@billease.ph>
2026-06-19 16:58:55 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSRR37cSPX3g8O-Vmm6VqkvpdnyLL5gVU4tP66oj-cxvNrfww6NEqDzoX070qle6w/pub?gid=2043204496&single=true&output=csv";
-
 Papa.parse(SHEET_URL, {
   download: true,
   header: true,
@@ -7,8 +5,14 @@ Papa.parse(SHEET_URL, {
 
   complete: function(results) {
 
-    console.log("FIRST ROW:");
-    console.log(results.data[0]);
+    const rows = results.data;
+
+    const validRows = rows.filter(
+      row => row["Campaign Name"] && row["Campaign Name"].trim() !== ""
+    );
+
+    console.log("FIRST VALID ROW:");
+    console.log(validRows[0]);
 
   }
 });
