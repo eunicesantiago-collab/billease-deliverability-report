e7eb44c980253db32e601e59391afd1b9be5e618
eunicesantiago-collab <eunice.santiago@billease.ph>
2026-06-19 17:00:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSRR37cSPX3g8O-Vmm6VqkvpdnyLL5gVU4tP66oj-cxvNrfww6NEqDzoX070qle6w/pub?gid=2043204496&single=true&output=csv";
+
 Papa.parse(SHEET_URL, {
   download: true,
   header: true,
